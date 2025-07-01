bed5dadafcf13b6fe71c85e50d1af027ac2b4416
pdraol <35792349+pdraol@users.noreply.github.com>
2025-07-01 15:21:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/webhook', (req, res) => {
 app.post('/webhook', (req, res) => {
   let bodyMess = req.body
   console.log(JSON.stringify(bodyMess, null, 2))
-  axios.post('https://whasapp.in/report-webhook.php', bodyMess,{ timeout: 3600000 })
+  axios.post('https://whasapp.in/report-webhook.php', bodyMess)
   .then((response) => {
     console.log("PHP response:", response.data)
   })
